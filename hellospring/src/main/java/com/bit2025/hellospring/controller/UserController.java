9f31922b31f6230d7bcc475e9af8be843c2065dd
Yuna-commits <ksm13563661@gmail.com>
2025-09-09 10:54:40 +0900

    Interceptor 연습

--- hellospring/src/main/java/com/bit2025/hellospring/controller/UserController.java
+++ hellospring/src/main/java/com/bit2025/hellospring/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserController {
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join() {
+		System.out.println("UserController.join() called [GET]");
 		return "/WEB-INF/views/join.jsp";
 	}
 
