d9f5c82a918835c2c9ff8e242ef94e3e9bbb3ba9
Yuna-commits <ksm13563661@gmail.com>
2025-09-16 16:28:51 +0900

    bean wiring #finish

---
+++ bean-wiring/src/main/java/config/videosystem/mixing/DVDPlayerConfig.java
@@ -0,0 +1,22 @@
+package config.videosystem.mixing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import videosystem.Avengers;
+import videosystem.DVDPlayer;
+
+/**
+ * 					import
+ * DVDPlayerConfig <------- DVDConfig
+ */
+@Configuration
+@Import({ DVDConfig.class })
+public class DVDPlayerConfig {
+
+	@Bean
+	public DVDPlayer dvdPlayer(Avengers dvd) {
+		return new DVDPlayer(dvd);
+	}
+}
